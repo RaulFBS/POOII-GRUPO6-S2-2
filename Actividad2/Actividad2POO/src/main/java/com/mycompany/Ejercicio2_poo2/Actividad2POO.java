2e574626fb80963de8e5f3171cb0bd3b5357b52f
raul <rs6626808@gmail.com>
2026-08-28 11:30:08 -0500

    Actividad 2 ejercicio 2 Completado por Said

---
+++ Actividad2/Actividad2POO/src/main/java/com/mycompany/Ejercicio2_poo2/Actividad2POO.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.Ejercicio2_poo2;
+
+/**
+ *
+ * @author saidm
+ */
+public class Actividad2POO {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
